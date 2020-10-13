f4c892578e9ddf31e3176fdd94125e3e16cc1f7b
李洁 <mayjleej@126.com>
2020-10-13 11:00:06 +0800

    第一次提价

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <h2> 1 </h2>
+
+    <h2>Ecosystem</h2>
+    <button @click="add">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg:' Eos toa bpp',
+    }
+  },
+  methods:{
+    add(){
+      console.log('122');
+    }
+  },
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
