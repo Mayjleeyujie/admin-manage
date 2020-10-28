3cab8ba7231b1f07897a5c61ea41cee7d8e4ad60
李洁 <mayjleej@126.com>
2020-10-28 17:34:57 +0800

    路由配置更改

--- src/page/admin/excel.vue
+++ src/page/admin/excel.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="hello">
-    报表管理
+    报表管理123123132
   </div>
 </template>
 
 <script>
 export default {
+  name:'excel',
   data () {
     return {
     }
--- src/router/index.js
+++ src/router/index.js
@@ -7,26 +7,22 @@ const originalPush = Router.prototype.push
 Router.prototype.push = function push(location) {
    return originalPush.call(this, location).catch(err => err)
 }
-
-// var defaultRouter=[]
-
 const defaultrouteList = [
   {
     path: '/',
-    component: home,
+    component: "home",
     leaf: true,  
     children: [
       {
         path: '/',
+        redirect: '/admin/dashed',
         name: '首页',
-        // component: 'admin/dashed'
-        component: () => import('@/page/admin/dashed.vue'),
+        component: 'admin/dashed'
       },
       {
         path: '/excel',
         name: '报表',
-        // component: 'admin/excel',
-        component: () => import('@/page/admin/excel.vue'),
+        component: 'admin/excel'
       }]
     },
   ]
@@ -47,20 +43,17 @@ export function routerMenuFun(defaultrouteList){
            name:item.name,
            hide:item.hide,//二级隐藏或显示
            meta:item.meta,
-          //  component:() => import(`@/page/${item.component}.vue`)
-          component:item.component
+           component:() => import(`@/page/${item.component}.vue`)
         })
       })
       routerList.push(obj)
   })
-  console.log(routerList,'routerList')
+  // console.log(routerList,'routerList')
   return routerList
 }
 localStorage.setItem('defaultrouteList',JSON.stringify(defaultrouteList));
 var defaultRouter = routerMenuFun(JSON.parse(localStorage.getItem("defaultrouteList")))
 // console.log(defaultRouter,'defaultRouter')
-
-// localStorage.removeItem("defaultrouteList")
 
 Vue.use(Router)
 
